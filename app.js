1bbe86b4856e348c05d2b2944304e8a2af0e027d
acastron22 <arielcastron22@hotmail.com>
2022-05-04 15:16:15 -0300

    instalação do nodemon e chamada de algumas rotas

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(<h1>Minha lista de tarefas!</h1>);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor foi iniciado");
+});
+
+// Faz a instalação do nodemon para aas atualizações sejam automáticas
+
